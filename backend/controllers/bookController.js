d129c12d494a4a6146cf4b4dbf3d7860109755a7
Santiago Julian <sjulian983@gmail.com>
2022-03-20 19:59:28 +0100

    home route refactored, controller in use

---
+++ backend/controllers/bookController.js
@@ -0,0 +1,16 @@
+const Book = require("../model/Book");
+
+const getAllBooks = async (req, res, next) => {
+  let books;
+  try {
+    books = await Book.find();
+  } catch (err) {
+    console.log(err);
+  }
+  if (!books) {
+    return res.status(404).json({ message: "No Books found" });
+  }
+  return res.status(200).json({ books });
+};
+
+exports.getAllBooks = getAllBooks;
--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -1,18 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const Book = require("../model/Book");
+const bookController = require("../controllers/bookController");
 
-router.get("/", async (req, res, next) => {
-  let books;
-  try {
-    books = await Book.find();
-  } catch (err) {
-    console.log(err);
-  }
-  if (!books) {
-    return res.status(404).json({ message: "No Books found" });
-  }
-  return res.status(200).json({ books });
-});
+router.get("/", bookController.getAllBooks);
 
 module.exports = router;
