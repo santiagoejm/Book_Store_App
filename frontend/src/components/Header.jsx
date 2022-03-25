a8a94e0819cc74b0dcb3de0ce146dfe49556e65d
Santiago Julian <sjulian983@gmail.com>
2022-03-25 12:27:36 +0100

    header component and router

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { AppBar, Toolbar, Typography, Tabs, Tab } from "@mui/material";
+import CollectionsBookmarkIcon from "@mui/icons-material/CollectionsBookmark";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header</div>;
+  const [value, setValue] = useState(0);
+
+  return (
+    <div>
+      <AppBar sx={{ backgroundColor: "#030303" }} position="sticky">
+        <Toolbar>
+          <Typography>
+            <CollectionsBookmarkIcon />
+          </Typography>
+          <Tabs
+            sx={{ ml: "auto" }}
+            textColor="inherit"
+            indicatorColor="secondary"
+            value={value}
+            onChange={(e, val) => setValue(val)}
+          >
+            <Tab LinkComponent={NavLink} to="/add" label="Add Book" />
+            <Tab LinkComponent={NavLink} to="/books" label="Books" />
+            <Tab LinkComponent={NavLink} to="/about" label="About Us" />
+          </Tabs>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 };
 
 export default Header;
