ed8bcb0a8e65754bdd4551e66ad0209df94d7d4d
Santiago Julian <sjulian983@gmail.com>
2022-03-28 15:13:54 +0200

    addbook page styled

--- frontend/src/components/AddBook.jsx
+++ frontend/src/components/AddBook.jsx
@@ -1,7 +1,47 @@
+import { FormLabel, TextField, Box, Button } from "@mui/material";
 import React from "react";
 
 const AddBook = () => {
-  return <div>AddBook</div>;
+  return (
+    <form>
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        maxWidth="700px"
+        alignContent="center"
+        marginLeft="auto"
+        marginRight="auto"
+        marginTop="20px"
+      >
+        <FormLabel>Name</FormLabel>
+        <TextField margin="normal" fullWidth variant="outlined" name="name" />
+
+        <FormLabel>Author</FormLabel>
+        <TextField margin="normal" fullWidth variant="outlined" name="author" />
+
+        <FormLabel>Description</FormLabel>
+        <TextField
+          margin="normal"
+          fullWidth
+          variant="outlined"
+          name="description"
+        />
+
+        <FormLabel>Price</FormLabel>
+        <TextField
+          type="number"
+          margin="normal"
+          fullWidth
+          variant="outlined"
+          name="price"
+        />
+        <Button variant="contained" type="submit">
+          Add Book
+        </Button>
+      </Box>
+    </form>
+  );
 };
 
 export default AddBook;
