0e7a20a4e8612e5d6c8a5f7cbc34bd93f1b7a263
Santiago Julian <sjulian983@gmail.com>
2022-04-06 18:20:39 +0200

    deleting books in place

--- frontend/src/components/Book/Book.jsx
+++ frontend/src/components/Book/Book.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { Button } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./book.css";
+import axios from "axios";
 
 const Book = (props) => {
+  const history = useNavigate();
   const { _id, name, author, description, price, image } = props.book;
+  const deleteHandler = async () => {
+    await axios
+      .delete(`http://localhost:3001/books/${_id}`)
+      .then((res) => res.data)
+      .then(() => history("/"))
+      .then(() => history("/books"));
+  };
 
   return (
     <div className="card">
@@ -16,7 +25,9 @@ const Book = (props) => {
       <Button LinkComponent={Link} to={`/books/${_id}`} sx={{ mt: "auto" }}>
         Update
       </Button>
-      <Button sx={{ mt: "auto" }}>Delete</Button>
+      <Button onClick={deleteHandler} sx={{ mt: "auto" }}>
+        Delete
+      </Button>
     </div>
   );
 };
