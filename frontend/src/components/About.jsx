4d620620325ab87185cf3d39cda9a134453894e3
Santiago Julian <sjulian983@gmail.com>
2022-04-07 18:46:49 +0200

    about us page  styled

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Box, Typography } from "@mui/material";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Typography sx={{ fontFamily: "fantasy" }} variant="h2">
+          This is a CRUD app
+        </Typography>
+        <Typography variant="h3">By MERN STACK</Typography>
+      </Box>
+    </div>
+  );
 };
 
 export default About;
